a28f7e46ecf4a508e86e2d2da19ab08cb6cf6cca
HiroyaNakakawaji <hiroya@procube.jp>
2023-05-16 14:03:02 +0900

    initial commit

---
+++ roles/prov-tools/files/swagger_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.file_status import FileStatus
+from swagger_server.models.result import Result
+from swagger_server.models.spool_list import SpoolList
