e287c18d8fb84e051782baffd4e385e6809db6be
HiroyaNakakawaji <hiroya@procube.jp>
2024-03-19 15:10:24 +0900

    ブラウザアクセス実装

---
+++ roles/session-manager/files/clean_chrome_container.py
@@ -0,0 +1,30 @@
+import json
+import os
+import requests
+import urllib
+from access_guacamole_api import generate_auth_token
+
+print("Started cleaning chrome containers")
+
+guacamole_url = os.environ.get('GUACAMOLE_URL')
+response_generate_auth_token = generate_auth_token()
+auth_token = response_generate_auth_token['authToken']
+guacamole_database = response_generate_auth_token['dataSource']
+get_connections_path = '/api/session/data/' + guacamole_database + '/connections?token=' + auth_token
+get_connections_url = guacamole_url + get_connections_path
+connections_res = requests.get(get_connections_url)
+connections_data = connections_res.json()
+
+for v in connections_data.values():
+  if v["idmIdentifier"] == "session-manager" and v["protocol"] == "vnc" and v["activeConnections"] == 0:
+    print("delete " + v["name"])
+    delete_url = 'http://localhost:80/delete'
+    params = {
+      "work_container": v["name"],
+      "vnc_identifier": v["identifier"]
+    }
+    params = json.dumps(params)
+    headers = {'Content-Type': 'application/json'}
+    requests.post(delete_url, data=params, headers=headers)
+    
+print("Ended cleaning chrome containers")
--- roles/session-manager/files/docker-entrypoint.sh
+++ roles/session-manager/files/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-exec /sbin/tini -- python /root/session-manager/main.py
+exec /usr/bin/supervisord
