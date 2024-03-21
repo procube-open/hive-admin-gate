22fd9c6ba6a1a3ece312ed934e807a64eb6de6d9
naoyaprocube <naoya@procube.jp>
2024-03-21 13:31:41 +0900

    disable when service exists

--- roles/enable-replication/templates/start-replication.sh
+++ roles/enable-replication/templates/start-replication.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
-# scpでlatestのバックアップファイルを/home/admin/backup配下に置く
+# scpでlatestのバックアップを/home/admin/backup配下に置く
+# for name in backup-idm-latest.tar.gz backup-idm_eu_app_csv-latest.tar.gz  backup-ldap-latest.ldif backup-ldap_config-latest.ldif backup-amdb-latest.sql.gz backup-guacamole_db-latest.sql.gz backup-mongo-backup-latest.tar.gz
+# do
+#   scp /home/admin/$name:/home/admin/$name
+# done
 
-for SERVICE in mongo idm
+for SERVICE in idm ldap amdb postgres mongo
 do
   hive-backup.sh -l $SERVICE -r
 done
