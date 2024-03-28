254eb25273914621fe4e2dec9560c93bc9de8163
HiroyaNakakawaji <hiroya@procube.jp>
2024-03-28 10:10:10 +0900

    SNMPトラップ設定変更

--- roles/addon/files/snmptrap.sh
+++ roles/addon/files/snmptrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dest="172.21.35.129"
-community=“public”
+dest="10.34.19.76"
+community=“k0ptr0”
 now=date
 
 #zabbix parameter
@@ -10,12 +10,41 @@ subject=$2
 body=$3
 
 declare -A selectOID=(
-["Problem: Service log-recorder restarted"]="1.3.6.1.4.1.35304.41.1.1"
-["Problem: Service log-recorder-web restarted"]="1.3.6.1.4.1.35304.41.2.1"
-["Problem: Service log-recorder replicas running percetage less than 100"]="1.3.6.1.4.1.35304.41.1.2"
-["Problem: Service log-recorder-web replicas running percetage less than 100"]="1.3.6.1.4.1.35304.41.2.2"
-["Problem: Exceed 90% Docker volume logdb_data in nslrdb usage"]="1.3.6.1.4.1.35304.41.3.1"
-["Problem: Exceed 90% Docker volume syslog_data in nslrdb usage"]="1.3.6.1.4.1.35304.41.4.1"
+["Problem: Service weebgate restarted"]="1.3.6.1.4.1.35304.42.1.1"
+["Problem: Service idm restarted"]="1.3.6.1.4.1.35304.42.2.1"
+["Problem: Service ldap restarted"]="1.3.6.1.4.1.35304.42.3.1"
+["Problem: Service am restarted"]="1.3.6.1.4.1.35304.42.4.1"
+["Problem: Service amdb restarted"]="1.3.6.1.4.1.35304.42.5.1"
+["Problem: Service guacamole restarted"]="1.3.6.1.4.1.35304.42.6.1"
+["Problem: Service guacd restarted"]="1.3.6.1.4.1.35304.42.7.1"
+["Problem: Service postgres restarted"]="1.3.6.1.4.1.35304.42.8.1"
+["Problem: Service file-server restarted"]="1.3.6.1.4.1.35304.42.9.1"
+["Problem: Service ftp-server restarted"]="1.3.6.1.4.1.35304.42.10.1"
+["Problem: Service sftpserver restarted"]="1.3.6.1.4.1.35304.42.11.1"
+["Problem: Service mongo restarted"]="1.3.6.1.4.1.35304.42.12.1"
+["Problem: Service webdav-serveer restarted"]="1.3.6.1.4.1.35304.42.13.1"
+["Problem: Service session-manager restarted"]="1.3.6.1.4.1.35304.42.14.1"
+["Problem: Service weebgate replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.1.2"
+["Problem: Service idm replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.2.2"
+["Problem: Service ldap replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.3.2"
+["Problem: Service am replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.4.2"
+["Problem: Service amdb replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.5.2"            
+["Problem: Service guacamole replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.6.2"       
+["Problem: Service guacd replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.7.2"           
+["Problem: Service postgres replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.8.2"        
+["Problem: Service file-server replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.9.2"     
+["Problem: Service ftp-server replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.10.2"     
+["Problem: Service sftpserver replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.11.2"     
+["Problem: Service mongo replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.12.2"          
+["Problem: Service webdav-serveer replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.13.2" 
+["Problem: Service session-manager replicas running percetage less than 100"]="1.3.6.1.4.1.35304.42.14.2"
+["Problem: Exceed 90% Docker volume idm_mysql in idm usage"]="1.3.6.1.4.1.35304.42.2.3"
+["Problem: Exceed 90% Docker volume idm_mongo in idm usage"]="1.3.6.1.4.1.35304.41.2.4"
+["Problem: Exceed 90% Docker volume ldap_data in ldap usage"]="1.3.6.1.4.1.35304.42.3.3"
+["Problem: Exceed 90% Docker volume amdb_data in amdb usage"]="1.3.6.1.4.1.35304.42.5.3"
+["Problem: Exceed 90% Docker volume guacamole-recordings in guacd usage"]="1.3.6.1.4.1.35304.42.7.3"
+["Problem: Exceed 90% Docker volume guacamole-db in postgres usage"]="1.3.6.1.4.1.35304.42.8.3"
+["Problem: Exceed 90% Docker volume mongo-db in mongo usage"]="1.3.6.1.4.1.35304.42.12.3"
 )
 
 if [ "$subject" != "" ]; then
