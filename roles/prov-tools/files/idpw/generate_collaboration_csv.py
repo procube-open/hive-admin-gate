32d2c91a0ba603543fb3e301ed92168227013ce0
HiroyaNakakawaji <126536104+HiroyaNakakawaji@users.noreply.github.com>
2024-06-14 03:47:37 +0000

    0614反映分

--- roles/prov-tools/files/idpw/generate_collaboration_csv.py
+++ roles/prov-tools/files/idpw/generate_collaboration_csv.py
@@ -18,7 +18,7 @@ if __name__=="__main__":
     user = user_req.json()
     user_group = user_group_req.json()
 
-    with open('user_group.csv', mode='w', encoding='utf-8', newline='') as file:
+    with open('/root/idpw/user_group.csv', mode='w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         for ug in user_group:
             row = [
@@ -28,7 +28,7 @@ if __name__=="__main__":
             ]
             writer.writerow(row)
 
-    with open('address_list.csv', mode='w', encoding='utf-8', newline='') as file:
+    with open('/root/idpw/address_list.csv', mode='w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         for ug in user_group:
             for ug_allow in ug['allowedAddressList'] if 'allowedAddressList' in ug else []:
@@ -46,7 +46,7 @@ if __name__=="__main__":
                 ]
                 writer.writerow(row)
     
-    with open('notification.csv', mode='w', encoding='utf-8', newline='') as file:
+    with open('/root/idpw/notification.csv', mode='w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         for ug in user_group:
             for ug_account_lock in ug['accountLockNotification'] if 'accountLockNotification' in ug else []:
@@ -85,7 +85,7 @@ if __name__=="__main__":
                 ]
                 writer.writerow(row)
 
-    with open('user.csv', mode='w', encoding='utf-8', newline='') as file:
+    with open('/root/idpw/user.csv', mode='w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         for u in user:
             if 'alignmentIDPW' in u and u['alignmentIDPW']:
@@ -101,7 +101,7 @@ if __name__=="__main__":
                 ]
                 writer.writerow(row)
     
-    with open('user_admin.csv', mode='w', encoding='utf-8', newline='') as file:
+    with open('/root/idpw/user_admin.csv', mode='w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         for u in user:
             if 'alignmentIDPW' in u and u['alignmentIDPW'] and (u['idmRole'] == 'IDM_USER_LEADER' or u['idmRole'] == 'IDM_USER_ADMIN' or u['idmRole'] == 'IDM_ADMIN'):
